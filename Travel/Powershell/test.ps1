88cdd98a77ee3a9a5231770f92e3643a28d560f4
ArunSahu1992 <31402954+ArunSahu1992@users.noreply.github.com>
2022-11-08 09:27:17 +0530

    Update test.ps1

--- Travel/Powershell/test.ps1
+++ Travel/Powershell/test.ps1
@@ -1,6 +1,7 @@
 $path = "C:\BussinessAPI"
 If(!(test-path -PathType container $path))
 {
+      import-module WebAdministration
       New-Item -ItemType Directory -Path $path
       Start-IISCommitDelay
       $TestSite = New-IISSite -Name BussinessAPI -BindingInformation "*:8121:" -PhysicalPath "C:\BussinessAPI" -Passthru
