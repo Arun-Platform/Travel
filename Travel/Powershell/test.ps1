0dde7ec74dc8f21535cb51218d2f7dfdba5678f4
ArunSahu1992 <31402954+ArunSahu1992@users.noreply.github.com>
2022-11-08 09:33:58 +0530

    Update test.ps1

--- Travel/Powershell/test.ps1
+++ Travel/Powershell/test.ps1
@@ -1,10 +1,10 @@
-$path = "C:\BussinessAPI"
+$path = "D:\BussinessAPI"
 If(!(test-path -PathType container $path))
 {
       import-module WebAdministration
       New-Item -ItemType Directory -Path $path
       Start-IISCommitDelay
-      $TestSite = New-IISSite -Name BussinessAPI -BindingInformation "*:8121:" -PhysicalPath "C:\BussinessAPI" -Passthru
-      $TestSite.Applications["/"].ApplicationPoolName = "ICM UI Website C7SP8"
+      $TestSite = New-IISSite -Name BussinessAPI -BindingInformation "*:8121:" -PhysicalPath "D:\BussinessAPI" -Passthru
+      $TestSite.Applications["/"].ApplicationPoolName = "DefaultAppPool"
       Stop-IISCommitDelay
 }
