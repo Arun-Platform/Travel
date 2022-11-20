7d3ba1c8b14d32f3f238504f7d8f1be0ae91e58f
ArunSahu1992 <31402954+ArunSahu1992@users.noreply.github.com>
2022-11-20 10:12:29 +0530

    Update test.ps1

--- Travel/test.ps1
+++ Travel/test.ps1
@@ -4,6 +4,6 @@ If(!(test-path -PathType container $path))
       New-Item -ItemType Directory -Path $path
       Start-IISCommitDelay
       $TestSite = New-IISSite -Name BussinessAPI -BindingInformation "*:8121:" -PhysicalPath "C:\BussinessAPI" -Passthru
-      $TestSite.Applications["/"].ApplicationPoolName = "ICM UI Website C7SP8"
+      $TestSite.Applications["/"].ApplicationPoolName = "DefaultAppPool"
       Stop-IISCommitDelay
 }
